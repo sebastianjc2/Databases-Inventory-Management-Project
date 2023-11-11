280ad9131886e2db386aa11e48184402bb5c5bcd
sebastianjc2 <josiassebastian@gmail.com>
2023-11-11 14:35:38 -0400

    Added and refactored the part handlers, routes, to handle the CRUD operation for parts

---
+++ Backend/DAOs/parts.py
@@ -0,0 +1,54 @@
+from Backend.config.dbconfig import pg_config
+import psycopg2
+
+
+class PartDAO:
+    def __init__(self):
+        # host = localhost is important!. If it's in heroku it would change. Also, the port could be important too!
+        connection_url = f'host = localhost dbname={(pg_config["dbname"])} user={pg_config["user"]} password={pg_config["password"]}'
+
+        print("Connection url:", connection_url)
+        self.conn = psycopg2.connect(connection_url) # connection URL to the DB to send queries
+
+    def getAllParts(self):
+        # here will be the query
+        cursor = self.conn.cursor()  # object utilized to send queries to the DB and too iterate through the results from the query
+        res = []
+        query = "select pid, pname, pcolor, pmaterial, msrp from parts"
+        cursor.execute(query)
+
+        for row in cursor:
+            print("row:", row)
+            res.append(row)  # adding the rows
+        return res
+
+    def searchByID(self, pid):
+        cursor = self.conn.cursor()
+        query = "SELECT pid, pname, pcolor, pmaterial, msrp FROM parts WHERE pid = %s"
+        cursor.execute(query, (pid,))
+        result = cursor.fetchone()
+        return result
+
+    def insertPart(self, name, color, material, msrp):
+        cursor = self.conn.cursor()
+        query = "INSERT INTO parts(pname, pcolor, pmaterial, msrp) VALUES (%s, %s, %s, %s) returning pid;"
+        cursor.execute(query, (name, color, material, msrp))  # passing the args so it's more secure
+        pid = cursor.fetchone()[0]
+        self.conn.commit()  # to save the changes
+        return pid
+
+    def deleteByID(self, pid):
+        cursor = self.conn.cursor()
+        query = "DELETE FROM parts WHERE pid = %s"
+        cursor.execute(query, (pid,))
+        count = cursor.rowcount
+        self.conn.commit()  # to save the changes
+        return count
+
+    def updateByID(self, pid, name, color, material, msrp):
+        cursor = self.conn.cursor()
+        query = "UPDATE parts set pname=%s, pcolor=%s, pmaterial=%s, msrp=%s where pid = %s;"
+        cursor.execute(query, (name, color, material, msrp, pid,))  # passing the args so it's more secure
+        count = cursor.rowcount
+        self.conn.commit()  # to save the changes
+        return count
---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+from Backend.handler.parts import PartHandler
+
+app = Flask(__name__)
+
+# apply CORS
+CORS(app)
+
+
+@app.route('/') # default route handler
+def greeting():
+    return 'Hello, this is the parts DB app practice'
+
+
+# route to get all parts or add a part
+@app.route('/parts', methods=['GET', 'POST'])
+def getAllParts():
+    if request.method == "GET":
+        return PartHandler().getAllParts()
+    elif request.method == "POST":  # performs insert queries
+        data = request.json
+        return PartHandler().insertPart(data)
+    else:
+        return jsonify('Not supported'), 405
+
+
+# route to find a specific part
+@app.route('/parts/<int:pid>', methods=['GET', 'PUT', 'DELETE'])
+def searchPartByID(pid):
+    if request.method == "GET":  # performs select-project-join queries
+        return PartHandler().searchByID(pid)
+    elif request.method == "PUT":  # performs update queries
+        data = request.json
+        return PartHandler().updateByID(pid, data)
+    elif request.method == "DELETE":  # performs delete queries
+        return PartHandler().deleteByID(pid)
+    else:
+        return jsonify('Not supported'), 405
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
