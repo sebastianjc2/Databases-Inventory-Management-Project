9b012b3389d6854fa9140f70fb06a8f514b6aca6
sebastianjc2 <josiassebastian@gmail.com>
2023-11-19 20:04:34 -0400

    Added the connection from psycopg2 to heroku

--- Backend/dbconfig.py
+++ Backend/dbconfig.py
@@ -1,8 +1,14 @@
 pg_config = {
-    'user': 'docker_admin',
-    'password': 'postgresadmin',
-    'dbname': 'sqlytes-inventory-app'
+    'user': 'rfuunoitsqrvhu',
+    'password': 'ad03f3262f921e6a03acbf5a2def6d79b298b9e3e732e33a641e359e141e69b3',
+    'dbname': 'dcfajr03gbu43b'
 }
+
+# pg_config = {
+#     'user': 'docker_admin',
+#     'password': 'postgresadmin',
+#     'dbname': 'sqlytes-inventory-app'
+# }
 
 # pg_config = {
 #     'user': 'user2',
