e3abeb34a9636e48e5fc84750bee198d8c7967e2
sebastianjc2 <josiassebastian@gmail.com>
2023-12-02 11:45:17 -0400

    Changed racks to be able to be added 'empty' (without having parts or being in a warehouse)

--- Backend/handler/racks.py
+++ Backend/handler/racks.py
@@ -22,19 +22,13 @@ class RackHandler:
         return jsonify(res)
 
     def addRack(self, data):
-        if len(data) != 5:
+        if len(data) != 2:
             return jsonify("Did not receive the correct amount of information needed for a Rack record. Need the "
-                           "following data: rack name (name), capacity, the warehouse id (wid) of the warehouse where "
-                           "the rack will be stored, a part id (pid) for the part that the rack will hold and the " 
-                           "quantity (qty) of this part that the rack will hold (can't be bigger than its capacity)."
-                           ), 400
+                           "following data: rack name (name), capacity"), 400
 
         try:
             name = data["Name"]
             capacity = data["Capacity"]
-            wid = data["wid"]
-            pid = data["pid"]
-            parts_qty = data["qty"]
         except:
             return jsonify("Error: Invalid argument names!"), 400
 
@@ -44,42 +38,12 @@ class RackHandler:
             return jsonify(f"Error: The inputted capacity '{capacity}' is not a valid integer!"), 400
         if capacity <= 0:
             return jsonify(f"Error: The rack must have a capacity bigger than 0."), 400
-        if not isinstance(wid, int):
-            return jsonify(f"Error: The inputted warehouse id '{wid}' is not a valid integer!"), 400
-        if not isinstance(pid, int):
-            return jsonify(f"Error: The inputted part id '{pid}' is not a valid integer!"), 400
-        if not isinstance(parts_qty, int):
-            return jsonify(f"Error: The inputted part quantity '{parts_qty}' is not a valid integer!"), 400
-        if parts_qty > capacity:
-            return jsonify(f"Error: The amount of parts that this rack will hold ({parts_qty}) can't be bigger than "
-                           f"its capacity ({capacity})"), 400
-
-        part_exists = PartDAO().searchByID(pid)
-        if not part_exists:
-            return jsonify(f"Error: The inputted part id '{pid}' is not found in the database."), 400
-
-        warehouse_exists = WarehouseDAO().getWarehouseByID(wid)
-        if not warehouse_exists:
-            return jsonify(f"Error: The inputted warehouse id '{wid}' is not found in the database."), 400
-
-        # is part already in this warehouse?
-        part_in_warehouse = StoredInDAO().isPartInWarehouse(pid, wid)
-        if part_in_warehouse:
-            return jsonify(f"Error: Part {pid} is already in Warehouse {wid}, and there can only be 1 of the same part "
-                           f"per Warehouse"), 400
 
         if name and capacity:
             dao = RackDAO()
 
-            # does the name already exist?
-            name_exists = dao.name_exists(name)
-            if name_exists: # it would violate UNIQUE constraint, so give an error
-                return jsonify("Error: This name already exists. Please pick another one."), 400
-
             rid = dao.addRack(name, capacity)
             data['rid'] = rid
-            # if everything above went right, we have to add this record to the stored_in table
-            StoredInDAO().modify_quantity(wid, pid, rid, parts_qty)
             return jsonify(data), 201
         else:
             return jsonify("Unexpected attribute values."), 400
@@ -124,7 +88,11 @@ class RackHandler:
         if not isinstance(capacity, int):
             return jsonify(f"Error: The inputted capacity '{capacity}' is not an integer!"), 400
 
-        # check how many parts is this rack currently holding
+        # check if capacity is valid
+        if capacity <= 0:
+            return jsonify(f"Error: The rack must have a capacity bigger than 0."), 400
+
+        # check how many parts is this rack currently holding (if it is in stored in. If it's not, qty will be 0)
         qty = StoredInDAO().get_qty_with_rid(rid)
         if capacity < qty:
             return jsonify(f"Error: Cannot update capacity since this new capacity ({capacity}) is less than the amount"
