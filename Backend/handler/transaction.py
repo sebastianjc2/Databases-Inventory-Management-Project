10fe9174aac082de53e5823588f6eac69e057a25
Pedro <pedrocpt2001@icloud.com>
2023-12-09 12:40:28 -0400

    Implemented All transactions by warehouse view

--- Backend/handler/transaction.py
+++ Backend/handler/transaction.py
@@ -2,17 +2,7 @@ from Backend.DAOs.transaction import TransactionDAO
 from flask import jsonify
 
 
-class TransactionHandler:
-    def mapToDict(self, tup):
-        my_dict = {}
-        my_dict["tid"] = tup[0]
-        my_dict["tdate"] = tup[1]
-        my_dict["part_amount"] = tup[2]
-        my_dict["pid"] = tup[3]
-        my_dict["uid"] = tup[4]
-        my_dict["wid"] = tup[5]
-        return my_dict
-    
+class TransactionHandler:    
     def mapToDictWithType(self, tup):
         my_dict = {}
         my_dict["tid"] = tup[0]
@@ -31,7 +21,7 @@ class TransactionHandler:
         if dbtuples:
             result = []
             for tup in dbtuples:
-                result.append(self.mapToDict(tup))
+                result.append(self.mapToDictWithType(tup))
             return jsonify(Result=result)
         else:
             return jsonify("Internal Server Error: Failed to load transactions"), 500
@@ -41,7 +31,10 @@ class TransactionHandler:
         dao = TransactionDAO()
         dbtuples = dao.getTransactionByID(tid)
         if dbtuples:
-            return jsonify(Result=self.mapToDict(dbtuples[0]))
+            result = []
+            for tup in dbtuples:
+                result.append(self.mapToDictWithType(tup))
+            return jsonify(Result=result)
         else:
             return jsonify("Failed to find matching transaction"), 404
 
