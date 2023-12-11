69afd3191e91ede64618fa485504abd31f977201
Mercrist <yariel.then@gmail.com>
2023-12-11 00:50:36 -0400

    RAHHHHH LOCAL STATS DONE

--- Backend/dbconfig.py
+++ Backend/dbconfig.py
@@ -1,19 +1,13 @@
+# pg_config = {
+#     'host': 'ec2-34-193-110-25.compute-1.amazonaws.com',
+#     'user': 'rfuunoitsqrvhu',
+#     'password': 'ad03f3262f921e6a03acbf5a2def6d79b298b9e3e732e33a641e359e141e69b3',
+#     'dbname': 'dcfajr03gbu43b'
+# }
+
 pg_config = {
-    'host': 'ec2-34-193-110-25.compute-1.amazonaws.com',
-    'user': 'rfuunoitsqrvhu',
-    'password': 'ad03f3262f921e6a03acbf5a2def6d79b298b9e3e732e33a641e359e141e69b3',
-    'dbname': 'dcfajr03gbu43b'
+    'host': 'localhost',
+    'user': 'docker_admin',
+    'password': 'postgresadmin',
+    'dbname': 'sqlytes-inventory-app'
 }
-
-# pg_config = {
-#     'host': 'localhost',
-#     'user': 'docker_admin',
-#     'password': 'postgresadmin',
-#     'dbname': 'sqlytes-inventory-app'
-# }
-
-# pg_config = {
-#     'user': 'user2',
-#     'password': 'user2',
-#     'dbname': 'db'
-# }
